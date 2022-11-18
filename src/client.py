5b59571a2250057558ecab59464c2afe0b1cc181
Rotmanss <romanromapev@gmail.com>
2022-11-18 23:20:40 +0200

    All work was done!

--- src/client.py
+++ src/client.py
@@ -8,22 +8,31 @@ class Client:
         self.Port = 1026
 
         self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.getLogger("Client")
+        logging.basicConfig(filename="client_log.log",
+                            level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filemode='w')
+
         self.run_client()
 
     def run_client(self):
+        logging.info("Client socket created")
         try:
             self.client_sock.connect((self.Host, self.Port))
-            init_msg = self.client_sock.recv(256).decode('utf-8')
+            logging.info(f'Connected to a server {self.Host}')
+            init_msg = self.client_sock.recv(400).decode('utf-8')
             print(init_msg)
 
             while True:
                 message = input('\ntype smt:\n')
                 self.client_sock.send(message.encode('utf-8'))
+                logging.info(f'Sent request: {message}')
 
                 data = self.client_sock.recv(256).decode('utf-8')
+                logging.info(f'Received answer: {data}')
 
-                if 'get_strings' in message or '=' in message:
+                if 'get_strings' in message or '=' in message and 'Exception' not in data:
                     strings_list = [x for x in data.strip('[]').split(', ')]
+                    print('Answer:')
                     for i, value in enumerate(strings_list):
                         value = value.replace('\'', '')
                         print(f'{i})', value.strip())
@@ -38,6 +47,7 @@ class Client:
             print('Connection closed.')
         except Exception as exp:
             print('Exception :', exp)
+            logging.exception(f"Exception: {exp}. The server may be down.")
 
 
 def main():
--- src/server.py
+++ src/server.py
@@ -11,39 +11,57 @@ class Server:
         self.client = None
         self.strings_to_edit = \
             ['Hello World!', 'Goodbye Space!', 'I like pasta', 'Please enter your name', 'My name is Anton']
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.getLogger("Server")
+        logging.basicConfig(filename="server_log.log", level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s', filemode='a')
+
         self.run_server()
 
     def run_server(self):
+        logging.info("Server was run")
         self.connect()
         self.sock.listen(1)
         print('Waiting for client :', self.sock.getblocking())
 
         try:
             self.client, addr = self.sock.accept()
-            print('Connected to:', addr)
+            print('Connected to a client:', addr)
+            logging.info(f'Connected to a client {addr}')
             self.client.send(self.initial_message().encode('utf-8'))
+            logging.info("Sent info")
             print('Listening to the client...')
+            logging.info('Listening to the client')
 
             while True:
                 client_msg = self.client.recv(256).decode('utf-8')
                 print('Client\'s message :', client_msg)
+                logging.info(f'Client\'s message: {client_msg}')
 
                 try:
                     answer = str(self.handle_client_msg(client_msg))
                     if 'stop' in answer:
                         answer += self.final_message()
                         self.client.send(answer.encode('utf-8'))
+                        logging.info(f'Send answer: {answer}')
+
+                        logging.info("Stop marker founded.")
+                        logging.info("Connection closed.")
+                        logging.info("\n" + "-" * 30 + "\n")
                         break
 
                     self.client.send(answer.encode('utf-8'))
+                    logging.info(f'Sent answer: {answer}')
 
                 except Exception as exp:
                     print('Exception:', exp)
+                    logging.warning(f'Exception: {exp}')
                     break
 
         except Exception as exp:
             print('Exception', exp)
+            logging.info("Error. Connection closed.")
             self.client.send('stop'.encode('utf-8'))
 
         finally:
@@ -51,13 +69,16 @@ class Server:
                 self.client.close()
             except Exception as exp:
                 print('Exception', exp)
+                logging.warning(f'Exception: {exp}')
                 print('Connection ended.')
+                logging.info("Server stopped!\n" + "-" * 30 + "\n")
 
     def connect(self):
         try:
             self.sock.bind((self.Host, self.Port))
         except OSError:
             print("Host is used.")
+            logging.warning('Host is used.')
             exit()
 
     def handle_client_msg(self, msg):
@@ -76,7 +97,7 @@ class Server:
             return 'Empty request'
 
     def parse_request(self, msg):
-        pattern = re.compile(r"([0-4])\:(\d*)\-\>(\w{0,30})")
+        pattern = re.compile(r"([0-4])\s*\:\s*(\d*)\s*\-\>\s*(\w{0,30})")
         it = re.match(pattern, msg)
         if it:
             pure_msg = it.group(0, 1, 2, 3)
@@ -91,16 +112,25 @@ class Server:
                 self.strings_to_edit[str_index] = "".join(current)
             except Exception as exp:
                 print('Exception:', exp)
+                logging.warning(f'Exception: {exp}')
                 return f'Exception: {exp}'
         else:
-            print('Wrong string index')
-            return 'Wrong string index'
+            print('Exception : Wrong string index or incorrect expression')
+            logging.warning(f'Exception : Wrong string index or incorrect expression')
+            return 'Exception : Wrong string index or incorrect expression'
 
         return self.strings_to_edit
 
     @staticmethod
     def initial_message():
-        return 'You can edit given strings, total string amount is 5'
+        return "Write a command from the list below, this will be sent to the server.\n" \
+               "Command list:\n\t" \
+               "1)'who' : returns information about author.\n\t" \
+               "2)'get_strings' : returns strings for editing, to edit string use command '3)'\n\t" \
+               "3)'=string index' : 'char index in selected string' -> 'your char(s)'\n\t" \
+               "P.S: You can add <= 30 chars, everything above 30 will be ignored!\n\t" \
+               "Example: =0:9->G\n\t" \
+               "4)'stop' : stops the session"
 
     @staticmethod
     def final_message():
